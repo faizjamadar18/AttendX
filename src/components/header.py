d016fe036adea5356d437205f0e0ab78235f705a
faizjamadar18 <faizjamadar18@gmail.com>
2026-04-23 17:57:47 +0530

    home screen

---
+++ src/components/header.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+def header_home():
+
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    
+    st.markdown(f"""
+        <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:10px;">
+            <img src='{logo_url}' style='height:100px;' />
+            <h1 style='text-align:center; color:#E0E3FF'>AttendeX</h1>
+        </div>   
+                
+                """, unsafe_allow_html=True)
